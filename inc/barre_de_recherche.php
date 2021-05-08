d52d5a945a5a2060454b5bf63e3209088f7eb5b8
eric-gilles <eric.gilles@etu.umontpellier.fr>
2021-05-08 23:09:27 +0200

    Update barre_de_recherche.php

--- inc/barre_de_recherche.php
+++ inc/barre_de_recherche.php
@@ -1,22 +1,29 @@
+<!-- Barre de Recherche -->
 <div class="container">
     <div class="row mb-5">
-    <div class="col-lg-7 mx-auto">
-      <div class="bg-white p-5 rounded shadow">
+        <div class="col-lg-7 mx-auto">
+            <div class="bg-white p-5 rounded shadow">
 
-        <!-- Underlined search bars with buttons -->
-        <form method="POST" action="./produits.php">
-          <div class="row justify-content-center">
-            <div class="form-group col-md-9">
-              <input id="exampleFormControlInput5" type="search" placeholder="Rechercher une voiture" name="search"class="form-control form-control-underlined">
-            </div>
-            <div class="form-group col-md-9">
-              <button type="submit" class="btn btn-primary rounded-pill btn-block shadow-sm">Rechercher</button>
-            </div>
-          </div>
-        </form>
-        <!-- End -->
+                <!-- Formulaire de recherche produit -->
+                <form method="POST" action="./produits.php">
+                    <div class="row justify-content-center">
 
-      </div>
+                         <!-- Input Champ de recherche -->
+                        <div class="form-group col-md-9">
+                            <input id="exampleFormControlInput5" type="search" placeholder="Rechercher une voiture" name="search"class="form-control form-control-underlined">
+                        </div>
+
+                        <!-- Bouton  envoyer -->
+                        <div class="form-group col-md-9">
+                            <button type="submit" class="btn btn-primary rounded-pill btn-block shadow-sm">Rechercher</button>
+                        </div>
+
+                    </div>
+                </form>
+                <!-- Fin du Formulaire de recherche produit -->
+
+            </div>
+        </div>
     </div>
-  </div>
 </div>
+<!-- Fin de la Barre de Recherche -->
