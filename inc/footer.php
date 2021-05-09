fa3078e2c85e0f881fef24d15b4cd5708e0080c3
eric-gilles <eric.gilles@etu.umontpellier.fr>
2021-05-09 15:28:45 +0200

    update produits

--- inc/footer.php
+++ inc/footer.php
@@ -19,12 +19,12 @@
     <div class="row">
       <!--Petit résumé de l'entreprise-->
       <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase" style='color:#005cbf;'>Nous somme :</h5>
+        <h5 class="text-uppercase" style='color:#005cbf;'>Nous sommes :</h5>
 
         <p style='color:#005cbf;'>
             Une entreprise française voulant proposer à nos clients des voitures de marque française.
-            Toute nos voiture sont livré avec les accessoire d'origines aisin que une traçabilité complète
-            de l'historique de la voiture. N'hésiter pas à nous contacter pour plus d'information
+            Toutes nos voitures sont livrés avec les accessoires d'origines ainsi qu'une traçabilité complète
+            de l'historique de la voiture. N'hésiter pas à nous contacter pour plus d'informations.
         </p>
       </div>
       <!-- Fin résumé-->
--- produits.php
+++ produits.php
@@ -9,18 +9,18 @@
 
 
 // inclus les fichiers
-require_once './model/DbManager.php';
-require_once './class/Voiture.php';
+	require_once './model/DbManager.php';
+	require_once './class/Voiture.php';
 ?><!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Les produits</title>
+	<title>Les produits</title>
 
 	<!-- Bootstrap & CSS -->
-    <link rel="stylesheet" href="./css/style.css">
+	<link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
@@ -29,12 +29,19 @@ require_once './class/Voiture.php';
   	<link rel="stylesheet" href="./css/mdb.min.css">
 </head>
 <body>
+	<div class="container-fluid h-100">
 	<?php
 		require_once './model/DbManager.php';
 		//require_once './inc/banner.php';
 		require_once './inc/navbar.php';
-		echo '<br><br>';
+		
+		echo "<br><br>\n<div class='row h-100'>";
+
+		require_once './inc/filtres.php';
+
 		require_once './inc/barre_de_recherche.php';
+
+		
 
 
 		if (isset($_POST['search']) && !is_null($_POST['search'])) {
@@ -46,33 +53,23 @@ require_once './class/Voiture.php';
 
 	?>
 	<br>
-	<aside></aside>
 	<div class="container">
-	 	<div class="row">
-	 		<div class="col-lg-3">
-	 			<h5>Marque</h5>
-	 			<hr>
-	 			<h6 class="text-info">Selectionner une marque</h6>
-	 			<ul class="list_group">
-	 				<?php  $sqlfilter = "SELECT ?>
-	 			</ul>
-	 		</div>
-
-		    <div class="col-sm">
+	 	<div class="row">		
+			<div class="col-sm">
 			<!-- 1ère Card Bootstrap -->
 				<div class="card" style="width: 18rem;">
 					<img class="card-img-top" src="./img/imgtest.png" alt="Card image cap">
 					<div class="card-body">
-			    		<h5 class="card-title">Card title</h5>
-			    		<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-			    		<a href="fiche_produit.php" class="btn btn-primary">Descriptif</a>
-			    		<button type="button" class="btn btn-light mr-1 mb-2">
-            				<i class="fas fa-shopping-cart pr-2"></i>Ajouter au Panier
-            			</button>
-			  		</div>
+				    	<h5 class="card-title">Card title</h5>
+				    	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    	<a href="fiche_produit.php" class="btn btn-primary">Descriptif</a>
+				    	<button type="button" class="btn btn-light mr-1 mb-2">
+	            			<i class="fas fa-shopping-cart pr-2"></i>Ajouter au Panier
+	            		</button>
+				  	</div>
 				</div>
 			<!-- Fin 1ère Card Bootstrap -->
-		    </div>
+			</div>
 
 	    	<div class="col-sm">
 			<!-- 2e Card Bootstrap -->
@@ -213,7 +210,8 @@ require_once './class/Voiture.php';
 		    
 		</div>
 	  </div>
-
+	</div>
+	</div>
 	<!-- Chargement JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
